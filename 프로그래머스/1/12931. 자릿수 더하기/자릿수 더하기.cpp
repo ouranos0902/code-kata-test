1d2815d35175490098a6338f9802b768f0c5f39b
허태린 <ouranos0902@gmail.com>
2025-12-29 09:24:30 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -6,10 +6,9 @@ int solution(int n)
    int answer = 0;
      while(n != 0)
      {
-         answer = answer+ n%10;
+         answer += n%10;
          n=n/10;
      }
     return answer;
 
-    return answer;
 }

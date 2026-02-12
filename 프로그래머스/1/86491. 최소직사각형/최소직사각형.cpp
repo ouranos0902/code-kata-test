de60e40ae8ffc99c394f187aec21031936aaee50
허태린 <ouranos0902@gmail.com>
2026-02-12 09:16:18 +0900

    Update 최소직사각형.cpp

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -5,14 +5,14 @@
 using namespace std;
 
 int solution(vector<vector<int>> sizes) {
-    int max_w = 0;
-    int min_h = 0;
+    int a = 0;
+    int b = 0;
     for(auto &card : sizes)
     {
         int w = max(card[0], card[1]);
         int h = min(card[0], card[1]);
-        max_w = max(max_w, w);
-        min_h = max(min_h, h);  
+        a = max(a, w);
+        b = max(b, h);  
     }
-    return max_w*min_h;
+    return a*b;
 }
